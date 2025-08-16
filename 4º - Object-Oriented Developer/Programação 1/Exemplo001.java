f182659330ffc7d8419e98931d620449a9cc2536
jrandrde <seuemail@example.com>
2025-08-16 15:52:52 -0300

    utilizando o Scanner p/ capturar entrada do teclado

---
+++ 4º - Object-Oriented Developer/Programação 1/Exemplo001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Exemplo001{
+    public static void main(String[] args){
+        int valor;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite um valor: ");
+        valor = teclado.nextInt();
+
+        System.out.println(valor/2);
+        System.out.println(valor%2);
+    }
+
+}
